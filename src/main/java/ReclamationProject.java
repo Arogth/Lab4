855d4c87aebdb93c49b1269cd7816404d8009c55
richard <gunter.ra@gmail.com>
2017-09-26 10:10:57 -0500

    done?

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -1,4 +1,4 @@
-/*
+/**
  * The following code needs a lot of TLC. So give it some!
  *
  * 1. Fix all checkstyle errors
@@ -6,21 +6,42 @@
  * 3. Improve the name of the methods and variables
  * 4. Add comments and Javadoc comments where needed
  * 5. Remove unnecessary comments as appropriate
+ * <p>
+ * <code>ReclamationProject.java</code>.
  */
+public class ReclamationProject {
+    /**
+    * @param a is a string.
+    * @param b is a string.
+    * @return the factorial of input if it is valid, or FACTORIAL_INVALID if it is not.
+    */
+    static String doit(final String a, final String b) {
 
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
-        /*
-         * For loop with i
-         */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+        String a2 = a, b2 = b;
+
+
+        if (a2.length() > b2.length()) {
+            String c = a2; // set c to a
+
+            a2 = b2;
+            b2 = c;
+        }
+
+        String r = "";
+
+        for (int i = 0; i < a2.length(); i++) {
+            for (int j = a2.length() - i; j > 0; j--) {
+                for (int k = 0; k < b2.length() - j; k++) {
+
+                    if (a2.regionMatches(i, b2, k, j) && j > r.length()) {
+
+                        r = a2.substring(i, i + j);
+                    }
+                }
+            }
+        }
+
+        return r;
+
+    }
 }
